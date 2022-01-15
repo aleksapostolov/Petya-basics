87860740604e9c2c8d5cc122beb4a15524511560
aleksapostolov <aleksandarvapostolov@gmail.com>
2022-01-15 21:54:41 +0200

    commit

--- More Exercice 1/05. Training Lab.js
+++ More Exercice 1/05. Training Lab.js
@@ -1,19 +1,13 @@
-function 
-
-
-
-
-
-trainingLab (input){
+function trainingLab (input){
     const dyljinaMetri = Number(input[0]);
     const shirinaMetri = Number(input[1]);
     const dyljinaSantimetri = dyljinaMetri * 100;
     const shirinaSantimetri = shirinaMetri * 100;
     const shirinaBezKoridor = shirinaSantimetri - 100;
-    const broiMasiShirina = shirinaBezKoridor / 70;
-    const broiMasiDyljina = dyljinaSantimetri / 120;
+    const broiMasiShirina = (Math.floor(shirinaBezKoridor / 70));
+    const broiMasiDyljina = (Math.floor (dyljinaSantimetri / 120));
     
-console.log (broiMasiShirina(Math.floor) + broiMasiDyljina(Math.floor)); 
+console.log  (broiMasiShirina * broiMasiDyljina -3); 
 }
 
 trainingLab (["15", "8.9"]);
