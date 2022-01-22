c803c6c66cb378aff8af66c1b5119e9eb1898c5b
aleksapostolov <aleksandarvapostolov@gmail.com>
2022-01-23 00:59:31 +0200

    commit

---
+++ Lesson 3/10. !!!!!! Invalid Number.js
@@ -0,0 +1,19 @@
+
+.................
+
+
+
+
+
+
+
+
+f/* unction invalidNumber(input){
+    const number = Number(input[0]);
+
+    let isValid = ((number >= 100 && number <= 200) || number === 0);
+if (!isValid) {
+    console.log ("invalid");
+    }
+}
+invalidNumber(["100"]) */
